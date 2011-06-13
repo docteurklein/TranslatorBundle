b7c5b1af66f956cb632ba547841d5c22239cf895
Klein Florian <florian.klein@free.fr>
2011-06-14 01:31:19 +0200

    added elements to interface

--- Dumper/DumperInterface.php
+++ Dumper/DumperInterface.php
@@ -2,7 +2,11 @@
 
 namespace Knplabs\Bundle\TranslatorBundle\Dumper;
 
+use Symfony\Component\Config\Resource\ResourceInterface;
+
 interface DumperInterface
 {
+    function supports(ResourceInterface $resource);
 
+    function update(ResourceInterface $resource, $id, $value);
 }
